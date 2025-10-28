13ac77642275ccc6ee99b793a10a6c41e9613d8d
Abdelkarim Anouer <aanouer@e2r3p13.1337.ma>
2025-10-28 15:16:24 +0100

    add file

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,8 @@
 #include "get_next_line.h"
 
-size_t	get_index_newline(char *data)
+static ssize_t get_index_newline(char *data)
 {
-	size_t	i;
+	ssize_t i;
 
 	i = 0;
 	while (data[i])
@@ -14,29 +14,57 @@ size_t	get_index_newline(char *data)
 	return (-1);
 }
 
-char	*get_next_line(int fd)
+static char *free_storage(char *storage, char *line)
 {
-	char		data[1024];
-	static char	*storage;
-	int	endline;
-	char	*line;
-	ssize_t	size;
+	if (storage && storage[0] != '\0')
+	{
+		line = ft_strdup(storage);
+		free(storage);
+		storage = NULL;
+		return (line);
+	}
+	return (NULL);
+}
 
-	if (!storage)
-		storage = ft_strdup("");
+static char	*get_line(char *storage, char *line, int fd, char *data)
+{
+	ssize_t	endline;
+	ssize_t	size;
+	char	*tmp;
+
 	size = 1;
 	while (size > 0)
 	{
 		size = read(fd, data, BUFFER_SIZE);
+		if (size < 0)
+			return (NULL);
 		data[size] = '\0';
+		tmp = storage;
 		storage = ft_strjoin(storage, data);
+		free(tmp);
 		endline = get_index_newline(storage);
 		if (endline != -1)
 		{
 			line = ft_substr(storage, 0, endline);
-			break;
+			storage = ft_strdup(storage + endline + 1);
+			return (line);
 		}
+		if (size == 0)
+			break;
 	}
-	storage = ft_strdup(storage + endline + 1);
-	return (line);
+	return (free_storage(storage, line));
+}
+
+char *get_next_line(int fd)
+{
+	char data[1024];
+	char *line;
+	static char *storage;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = NULL;
+	if (!storage)
+		storage = ft_strdup("");
+	return get_line(storage, line, fd, data);
 }
